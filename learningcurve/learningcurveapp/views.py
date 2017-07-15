039e96de213346ae061b99c78a31c91fba01e739
pranithkumar <praneeth970@gmail.com>
2017-07-16 00:19:15 +0530

    templates_commit

--- learningcurve/learningcurveapp/views.py
+++ learningcurve/learningcurveapp/views.py
@@ -5,6 +5,5 @@ from django.http import HttpResponse
 
 from django.template import Context, loader
 def index(request):
-    template=loader.get_template("learningcurveapp/sample.html")
+    template = loader.get_template("learningcurveapp/adminlogin.html")
     return HttpResponse(template.render())
-    #return render(request,'learningcurveapp/index.html','jfdksjafl')
