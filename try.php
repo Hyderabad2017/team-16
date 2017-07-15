ecacc0706d742368082e35e44085659b3ad85b2f
rishyareddy <maggi.spark@gmail.com>
2017-07-15 21:37:49 +0530

    classroom assesment

---
+++ try.php
@@ -0,0 +1,42 @@
+<?php>
+    $host="localhost";
+$dbuser="root";
+$dbpass="";
+$dbname="example";
+$con=mysqli_connect($host,$dbuser,$dbpass,$dbname);
+if(mysqli_conect_errno())
+{
+    die("connection failed");
+    
+}
+
+?>
+<html>
+<title>
+    <body>
+    
+  /*  $sql=select (coll,hand,comm,buil,self,empa,selfes,expr,selfaw);
+        $
+    */
+    
+    
+    
+    
+    
+    </body>
+    
+    
+    
+    
+    
+    
+    
+    </title>
+
+
+
+
+
+
+</html>
+mysqli_close($con);
