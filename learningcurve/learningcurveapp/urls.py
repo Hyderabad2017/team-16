2cf8581b151e1f9f223a71082b13ab09dfd0bef8
gouthamgundapu <gouthamgundapu36@gmail.com>
2017-07-16 09:30:18 +0530

    changes

--- learningcurve/learningcurveapp/urls.py
+++ learningcurve/learningcurveapp/urls.py
@@ -6,9 +6,8 @@ urlpatterns = [
     url(r'^home', views.home, name='home'),
     url(r'^adminlogin', views.adminlogin, name='adminlogin'),
     url(r'^teacherlogin', views.teacherlogin, name='teacherlogin'),
-    url(r'^teacherdetails', views.teacherdetails, name='teacherdetails')
-    url(r'^studentassessment',views.studentassessment, name='studentassessment'),
-    url(r'^studentdetails',views.studentdetails, name='studentdetails'),
+    url(r'^studentassessment', views.studentassessment, name='studentassessment'),
+    url(r'^studentdetails', views.studentdetails, name='studentdetails'),
     url(r'^finalreport', views.finalreport, name='finalreport'),
-    url(r'^teacherdetails',views.teacher_details,name='teacher_details')
+    url(r'^teacherdetails', views.teacherdetails, name='teacherdetails')
 ]
